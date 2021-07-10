03c957309d1ff83cf87bdd7f1db7088133f27477
Ekain06 <76905934+Ekain06@users.noreply.github.com>
2021-07-10 22:33:45 +0200

    Added server check to AtomicArmorTick

--- src/main/java/com/ekain06/neopi/mixin/AtomicArmorTick.java
+++ src/main/java/com/ekain06/neopi/mixin/AtomicArmorTick.java
@@ -17,9 +17,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
 public abstract class AtomicArmorTick {
+    private static final int STATUS_EFFECT_DURATION = 10;
     private static void addStatusEffect(AtomicArmorTick aat, StatusEffect se, int amplifier) {
         ((LivingEntityInvoker) aat).callAddStatusEffect(
-                new StatusEffectInstance(se, 5, amplifier, false, false, false));
+                new StatusEffectInstance(se, STATUS_EFFECT_DURATION, amplifier, false, false, false));
     }
 
     @Shadow public abstract ItemStack getEquippedStack(EquipmentSlot slot);
@@ -27,47 +28,50 @@ public abstract class AtomicArmorTick {
 
     @Inject(at = @At("TAIL"), method = "tick()V")
     private void tick(CallbackInfo ci) {
-        // TODO optimize
-        ItemStack head = this.getEquippedStack(EquipmentSlot.HEAD);
-        ItemStack chest = this.getEquippedStack(EquipmentSlot.CHEST);
-        ItemStack legs = this.getEquippedStack(EquipmentSlot.LEGS);
-        ItemStack feet = this.getEquippedStack(EquipmentSlot.FEET);
+        if (!((EntityAccessor) this).getWorld().isClient) {
+            // TODO optimize
+            ItemStack head = this.getEquippedStack(EquipmentSlot.HEAD);
+            ItemStack chest = this.getEquippedStack(EquipmentSlot.CHEST);
+            ItemStack legs = this.getEquippedStack(EquipmentSlot.LEGS);
+            ItemStack feet = this.getEquippedStack(EquipmentSlot.FEET);
 
-        // DONE is it better if this is an int?  -- most certainly for performance, but adds an extra 24 bytes for each player. Worth it IMHO.
-        int armorPieces = 0;
+            // DONE is it better if this is an int?  -- most certainly for performance, but adds an extra 24 bytes for each player. Worth it IMHO.
+            int armorPieces = 0;
 
-        if (head.isOf(AtomicArmorItems.Helmet.ITEM)) {
-            addStatusEffect(this, StatusEffects.WATER_BREATHING, 0);
-            addStatusEffect(this, StatusEffects.NIGHT_VISION, 0);
-            addStatusEffect(this, StatusEffects.DOLPHINS_GRACE, 0);
-            armorPieces++;
-        }
+            if (head.isOf(AtomicArmorItems.Helmet.ITEM)) {
+                addStatusEffect(this, StatusEffects.WATER_BREATHING, 0);
+                addStatusEffect(this, StatusEffects.NIGHT_VISION, 0);
+                addStatusEffect(this, StatusEffects.DOLPHINS_GRACE, 0);
+                armorPieces++;
+            }
 
-        if (chest.isOf(AtomicArmorItems.Chestplate.ITEM)) {
-            this.abilities.allowFlying = true;
-            armorPieces++;
-        } else if (this.abilities.allowFlying) {
-            this.abilities.allowFlying = false;
-        }
+            if (chest.isOf(AtomicArmorItems.Chestplate.ITEM)) {
+                this.abilities.allowFlying = true;
+                armorPieces++;
+            } else if (this.abilities.allowFlying) {
+                this.abilities.allowFlying = false;
+            }
 
-        if (legs.isOf(AtomicArmorItems.Leggings.ITEM)) {
-            this.abilities.invulnerable = true;
-            armorPieces++;
-        } else if (this.abilities.invulnerable) {
-            this.abilities.invulnerable = false;
-        }
+            if (legs.isOf(AtomicArmorItems.Leggings.ITEM)) {
+                this.abilities.invulnerable = true;
+                armorPieces++;
+            } else if (this.abilities.invulnerable) {
+                this.abilities.invulnerable = false;
+            }
 
-        if (feet.isOf(AtomicArmorItems.Boots.ITEM)) {
-            addStatusEffect(this, StatusEffects.SPEED, 5);  // this way we don't need to worry about "unsetting" the speed
-            addStatusEffect(this, StatusEffects.JUMP_BOOST, 2);
-            armorPieces++;
-        }
+            if (feet.isOf(AtomicArmorItems.Boots.ITEM)) {
+                addStatusEffect(this, StatusEffects.SPEED, 5);  // this way we don't need to worry about "unsetting" the speed
+                addStatusEffect(this, StatusEffects.JUMP_BOOST, 2);
+                armorPieces++;
+            }
 
-        if (armorPieces == 4) {  // aka full set's effects
-            addStatusEffect(this, StatusEffects.REGENERATION, 999);
-            //addStatusEffect(this, StatusEffects.ABSORPTION, 2);
-            //addStatusEffect(this, StatusEffects.HEALTH_BOOST, 5);  // useless since user is invulnerable
-            addStatusEffect(this, StatusEffects.SATURATION, 999);
+            if (armorPieces == 4) {  // aka full set's effects
+                addStatusEffect(this, StatusEffects.REGENERATION, 999);
+                // VVV these aren't needed since you're invulnerable anyways
+                //addStatusEffect(this, StatusEffects.ABSORPTION, 2);
+                //addStatusEffect(this, StatusEffects.HEALTH_BOOST, 5);  // useless since user is invulnerable
+                addStatusEffect(this, StatusEffects.SATURATION, 999);
+            }
         }
     }
 }
---
+++ src/main/java/com/ekain06/neopi/mixin/EntityAccessor.java
@@ -0,0 +1,12 @@
+package com.ekain06.neopi.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(Entity.class)
+public interface EntityAccessor {
+    @Accessor
+    World getWorld();
+}
